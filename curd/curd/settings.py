c5b61bf86b046ef9b8d5b072ce4c29eb75b01dba
Nuwan Chamikara <39343048+gmnchamikara@users.noreply.github.com>
2022-12-22 00:48:27 +0530

    model file

--- curd/curd/settings.py
+++ curd/curd/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'livereload',
             ],
         },
     },
@@ -126,3 +127,4 @@ MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
