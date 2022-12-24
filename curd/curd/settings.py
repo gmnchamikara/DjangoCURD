365cc918ff4fe87e6a6a0552c133e074d0506209
Nuwan Chamikara <39343048+gmnchamikara@users.noreply.github.com>
2022-12-24 16:29:23 +0530

    done

--- curd/curd/settings.py
+++ curd/curd/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'book'
+    'book',
+    'livereload',
 ]
 
 MIDDLEWARE = [
@@ -64,7 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'livereload',
+                
             ],
         },
     },
